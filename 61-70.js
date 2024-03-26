e6c5c9f603c2d577ff20fb2d7b23bd6ddceb30e7
AvilaRamiro <rn.avila.06@gmail.com>
2024-03-25 22:40:56 -0300

    listo

--- 61-70.js
+++ 61-70.js
@@ -1,26 +1,36 @@
 //61- Escribe una función llamada esPrimo que tome un número como argumento y retorne 
 //true si es primo o false si no lo es. Un número primo es aquel que solo es divisible
 //por 1 y por sí mismo
-function esPrimo (number){
+function esPrimo(number){
     
     let tieneResto = true;
 
-    for (i = 2; i <= (numero - 1); i++) {
-        if (numero % i == 0) {
+    for (i = 2; i <= (number - 1); i++) {
+        if (number % i == 0) {
             return false;
         }
     }
     return tieneResto;
 }
+esPrimo(number = 7)
+//let numero = 7;
 
-let numero = 7;
-
-console.log('Es el número', numero, 'primo?', esPrimo(numero));
+console.log('Es el número', number, 'primo?', esPrimo(number));
 
 //62- Define una función llamada invierteCadena que tome una cadena de texto como 
 //argumento y retorne una nueva cadena con los caracteres en orden inverso.
 
-funcion invierteCadena(cadena){
-    cadenaInvertida = '';
-    for (let i = cadena.length - 1; i >= 0; i--)
+function invierteCadena(cadena){
+
+    let cadenaString = numero.toString()
+    let cadenaInvertida = ""
+
+    for (let i = cadena.length - 1; i >= 0; i--){
+        cadenaInvertida = cadenaInvertida + cadenaString[i]
+    }
+
+    return cadenaInvertida
 }
+
+invierteCadena('Hola como estas')
+console.log(cadenaInvertida)
